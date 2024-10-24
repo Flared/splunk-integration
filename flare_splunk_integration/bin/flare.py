3865cd6703b83689d8a68337982006b725d4312b
Mark Kasaboski <mark.tech1@gmail.com>
2024-10-24 11:19:22 -0400

    Fixes ruff linting errors (#7)
    * Fixes ruff linting errors and addresses splunk-appinspect errors
    
    ---------
    
    Co-authored-by: Mark Kasaboski <mark.kasaboski@gmail.com>

--- flare_splunk_integration/bin/flare.py
+++ flare_splunk_integration/bin/flare.py
@@ -8,30 +8,31 @@ from vendor.requests.auth import AuthBase
 from urllib.error import HTTPError
 from vendor.flareio import FlareApiClient
 
-APP_NAME = 'flare_splunk_integration'
+APP_NAME = "flare_splunk_integration"
+
 
 class FlareAPI(AuthBase):
-
     def __init__(self, *, app):
         # Should be able to use app.service.storage_passwords.get(),
         # but I can't seem to get that to work. list() works.
-        server_key : Optional[str] = None
-        tenant_id : Optional[str] = None
+        server_key: Optional[str] = None
+        tenant_id: Optional[str] = None
         for item in app.service.storage_passwords.list():
-            if item.content.username == 'serverkey':
+            if item.content.username == "serverkey":
                 server_key = item.clear_password
 
-            if item.content.username == 'tenantid':
+            if item.content.username == "tenantid":
                 tenant_id = item.clear_password
 
         self.flare_endpoints = app.service.confs["flare"]["endpoints"]
         self.api_key = server_key
         self.tenant_id = tenant_id
-        self.flare_client = FlareApiClient(api_key=self.api_key,
-                                           tenant_id=self.tenant_id)
+        self.flare_client = FlareApiClient(
+            api_key=self.api_key, tenant_id=self.tenant_id
+        )
 
     def retrieve_feed(self, *, from_: Optional[str] = None) -> dict[str, Any]:
-        url=self.flare_endpoints["me_feed_endpoint"]
+        url = self.flare_endpoints["me_feed_endpoint"]
         response = self.flare_client.post(
             url=url,
             json={"lite": "true", "from": from_},
