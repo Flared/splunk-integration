a3af87aaa0e3e35e4c2df22aa5a7bc31091cb494
Marc-Antoine Hinse <marc.antoine@flare.io>
2024-10-29 11:04:29 -0400

    Remodeled application architure to use webpack to give ability to code in typescript and JSX React
    - Converted the static JSX-less configuration screen to React

--- README.md
+++ README.md
@@ -9,3 +9,32 @@ At the time of this writing, Splunk Enterprise is at version 9.3.0. This version
 ## Installation
 
 For now, see (this confluence page)[https://flareio.atlassian.net/wiki/spaces/engineering/pages/384401447/Splunk+Enterprise] for instructions on how to install and configure this Splunk integration with your Splunk Enterprise instance.
+
+## Architecture Overview
+
+The project contains a variety of packages that are published and versioned collectively. Each package lives in its own 
+directory in the `/packages` directory. Each package is self contained, and defines its dependencies in a package.json file.
+
+We use [Yarn Workspaces](https://yarnpkg.com/lang/en/docs/workspaces/) and [Lerna](https://github.com/lerna/lerna) for
+managing and publishing multiple packages in the same repository.
+
+
+## Development
+
+If you have your own Splunk Enterprise installed locally, you can use
+
+```
+make splunk-local
+```
+
+to download the required dependencies, watch the file changes to re-compile on the fly and generate a symlink between the Splunk app and the Splunk Enterprise applications folder.
+
+To be able to see your changes when refreshing, add this at the end of this file:
+```
+File: /Applications/Splunk/etc/system/local/web.conf
+
+[settings]
+cacheEntriesLimit = 0
+cacheBytesLimit = 0
+```
+Then restart your Splunk Enterprise instance.
---
+++ packages/configuration-screen/src/setupConfiguration.ts
@@ -0,0 +1,140 @@
+import { updateConfigurationFile } from './configurationFileHelper';
+import { Tenant } from './models/flare';
+import {
+    SplunkApplicationNamespace,
+    SplunkService,
+    SplunkStoragePasswordAccessors,
+} from './models/splunk';
+import { promisify } from './util';
+
+const appName: string = 'flare';
+const storageRealm: string = 'flare_integration_realm';
+const applicationNameSpace: SplunkApplicationNamespace = {
+    owner: 'nobody',
+    app: appName,
+    sharing: 'app',
+};
+
+async function completeSetup(splunkService: SplunkService): Promise<void> {
+    const configurationFileName = 'app';
+    const stanzaName = 'install';
+    const propertiesToUpdate = {
+        is_configured: 'true',
+    };
+
+    await updateConfigurationFile(
+        splunkService,
+        configurationFileName,
+        stanzaName,
+        propertiesToUpdate
+    );
+}
+
+async function reloadApp(splunkService: SplunkService): Promise<void> {
+    const splunkApps = splunkService.apps();
+    await promisify(splunkApps.fetch)();
+
+    const currentApp = splunkApps.item(appName);
+    await promisify(currentApp.reload)();
+}
+
+function redirectToHomepage(): void {
+    const redirectUrl = `/app/${appName}`;
+    window.location.href = redirectUrl;
+}
+
+function createService(applicationNamespace: SplunkApplicationNamespace): SplunkService {
+    // The splunkjs is injected by Splunk
+    // eslint-disable-next-line no-undef
+    const http = new splunkjs.SplunkWebHttp();
+    // eslint-disable-next-line no-undef
+    const service = new splunkjs.Service(http, applicationNamespace);
+
+    return service;
+}
+
+function retrieveUserTenants(
+    serverKey: string,
+    successCallback: (userTenants: Array<Tenant>) => void,
+    errorCallback: (errorMessage: string) => void
+): void {
+    const service = createService(applicationNameSpace);
+    const data = { serverKey };
+    service.post('/services/retrieve_user_tenants', data, (err, response) => {
+        if (err) {
+            errorCallback(err.data);
+        } else if (response.status === 200) {
+            successCallback(response.data.tenants);
+        }
+    });
+}
+
+function doesPasswordExist(storage: SplunkStoragePasswordAccessors, key: string) {
+    const passwordId = `${storageRealm}:${key}:`;
+
+    for (const password of storage.list()) {
+        if (password.name === passwordId) {
+            return true;
+        }
+    }
+    return false;
+}
+
+async function savePassword(
+    storage: SplunkStoragePasswordAccessors,
+    key: string,
+    value: string
+): Promise<void> {
+    const passwordExists = doesPasswordExist(storage, key);
+    if (passwordExists) {
+        const passwordId = `${storageRealm}:${key}:`;
+        await storage.del(passwordId);
+    }
+    return promisify(storage.create)({
+        name: key,
+        realm: storageRealm,
+        password: value,
+    });
+}
+
+async function saveConfiguration(serverKey: string, tenantId: number) {
+    const service = createService(applicationNameSpace);
+
+    const storagePasswords = await promisify(service.storagePasswords().fetch)();
+    await savePassword(storagePasswords, 'serverkey', serverKey);
+    await savePassword(storagePasswords, 'tenantid', `${tenantId}`);
+
+    await completeSetup(service);
+    await reloadApp(service);
+
+    redirectToHomepage();
+}
+
+async function retrievePassword(passwordKey: string): Promise<string> {
+    const service = createService(applicationNameSpace);
+    const storagePasswords = await promisify(service.storagePasswords().fetch)();
+    const passwordId = `${storageRealm}:${passwordKey}:`;
+
+    for (const password of storagePasswords.list()) {
+        if (password.name === passwordId) {
+            return password._properties.clear_password;
+        }
+    }
+    return '';
+}
+
+async function retrieveServerKey(): Promise<string> {
+    return retrievePassword('serverkey');
+}
+
+async function retrieveTenantId(): Promise<number> {
+    return retrievePassword('tenantid').then((tenantId) => {
+        if (tenantId !== '') {
+            return parseInt(tenantId, 10);
+        }
+
+        return -1;
+    });
+}
+
+export { saveConfiguration, retrieveUserTenants, retrieveServerKey, retrieveTenantId };
