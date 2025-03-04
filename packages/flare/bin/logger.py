11137a04fde3854f2011f809c983224c3c647cf3
Xavier Lavallee <xavier.lavallee@gmail.com>
2025-03-04 16:01:16 -0500

    Move local splunk to a dockerized setup

--- packages/flare/bin/logger.py
+++ packages/flare/bin/logger.py
@@ -10,22 +10,18 @@ from typing import Any
 class Logger:
     def __init__(self, *, class_name: str) -> None:
         splunk_home = os.environ.get("SPLUNK_HOME")
-        is_local_build = False
         log_filepath = ""
         if splunk_home:
             log_filepath = os.path.join(
                 splunk_home, "var", "log", "splunk", f"{APP_NAME}.log"
             )
-            application_folder = os.path.join(splunk_home, "etc", "apps", APP_NAME)
-            is_local_build = os.path.islink(application_folder)
         else:
             log_filepath = os.path.join(tempfile.gettempdir(), f"{APP_NAME}.log")
 
         self.tag_name = os.path.splitext(os.path.basename(class_name))[0]
         self._logger = logging.getLogger(f"flare-{self.tag_name}")
 
-        if is_local_build:
-            # If the application is a symlink, it's been installed locally
+        if os.environ.get("IS_LOCAL_BUILD"):
             self._logger.setLevel(logging.INFO)
         else:
             self._logger.setLevel(logging.ERROR)
